8024f9fbfb5074dbd355955a405667b38e0f305b
JasonRodriguezGarcia <jasonrodriguezempleo.gmail.com>
2025-01-26 19:23:36 +0100

    changing host to server web page

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ export default class App extends Component {
         super();
 
         this.state = {
-            hostAPP: "http://127.0.0.1:5000"
-            // hostAPP: "https://flask-server-capstone.onrender.com"
+            // hostAPP: "http://127.0.0.1:5000"
+            hostAPP: "https://flask-server-capstone.onrender.com"
         };
     }
     render() {
